0f0ab8ed6488a1711ae1dbccdb498c7647516291
João Ramos <jvd.ramos@hotmail.com>
2024-02-23 20:32:45 -0300

    style: added animation

--- src/components/LargeButton/LargeButton.tsx
+++ src/components/LargeButton/LargeButton.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {TouchableOpacityProps, View} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {Animated, TouchableOpacityProps, Easing} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import {ButtonTitle, Container} from './styles';
 
@@ -7,11 +7,36 @@ interface LargeButtonProps extends TouchableOpacityProps {
   loading: boolean;
 }
 
-const LargeButton: React.FC<LargeButtonProps> = ({loading = true, ...rest}) => {
+const LargeButton: React.FC<LargeButtonProps> = ({loading, ...rest}) => {
+  const rotation = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(rotation, {
+        toValue: 1,
+        duration: 2000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+    ).start();
+  }, []);
+
+  const spin = rotation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
   return (
     <Container loading={loading} disabled={loading} {...rest}>
       <ButtonTitle>{loading ? 'Acessando' : 'Acessar'}</ButtonTitle>
-      {loading && <Icon name="rotate-cw" size={24} color={'white'} />}
+      {loading && (
+        <Animated.View
+          style={{
+            transform: [{rotate: spin}],
+          }}>
+          <Icon name="rotate-cw" size={24} color={'white'} />
+        </Animated.View>
+      )}
     </Container>
   );
 };
