dcc4ea6c6856d03807815a9bb4de246e98d500a8
João Ramos <jvd.ramos@hotmail.com>
2024-02-23 20:31:34 -0300

    build: updating types

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,8 +1,13 @@
+import {IMovie} from '../config/functions/Movies/types';
+
 export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       HomeScreen: undefinded;
       Dashboard: undefinded;
+      MovieInfo: {
+        movie: IMovie;
+      };
     }
   }
 }
