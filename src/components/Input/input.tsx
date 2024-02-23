ec2b1b30297bfede81879c5bf3bb24e02453400f
João Ramos <jvd.ramos@hotmail.com>
2024-02-23 17:09:23 -0300

    feat: added input validation

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -3,23 +3,42 @@ import React from 'react';
 import {Container, ErrorMessage, InputText, InputTitle} from './styles';
 import {TextInputProps} from 'react-native';
 
+import {Control, Controller, FieldError} from 'react-hook-form';
+
 interface InputProps extends TextInputProps {
   title: string;
-  error?: boolean;
   errorMessage?: string;
+  control: Control<any>;
+  name: string;
+  error?: FieldError;
 }
 
 const Input: React.FC<InputProps> = ({
   title,
-  error = false,
+  name,
+  error,
   errorMessage,
+  control,
   ...rest
 }) => {
   return (
     <Container>
-      <InputTitle error={error} >{title}</InputTitle>
-      <InputText error={error} {...rest} />
-      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+      <Controller
+        name={name}
+        control={control}
+        render={({field: {onChange, value}}) => (
+          <>
+            <InputTitle error={!!error?.message}>{title}</InputTitle>
+            <InputText
+              error={!!error?.message}
+              onChangeText={onChange}
+              value={value}
+              {...rest}
+            />
+            {!!error?.message && <ErrorMessage>{error?.message}</ErrorMessage>}
+          </>
+        )}
+      />
     </Container>
   );
 };
